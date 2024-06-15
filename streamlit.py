5a99cc9f1da125deea508cfbd303e64b0dd8fb2e
Abhishek RT <136077817+Abhi-Shek17@users.noreply.github.com>
2024-06-15 16:42:00 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,9 +1,8 @@
 import streamlit as st
 from PIL import Image
 import torch
+import numpy as np
 from torchvision import transforms
-import requests
-import os
 
 # Define custom CSS for the background, title, and caption
 st.markdown(
@@ -66,56 +65,28 @@ st.markdown(
 )
 
 @st.cache_resource
-def download_model(url, model_path):
-#     # Download the model file
-#     # Download the model file
-#     response = requests.get(url)
-#     response.raise_for_status()  # Ensure the request was successful
-#     with open(model_path, 'wb') as f:
-#         f.write(response.content)
-#     # Load the model
-    for (root,dirs,files) in os.walk('.', topdown=True):
-        print (root)
-        print (len(dirs))
-        print (files)
-        print ('--------------------------------')
-    model = torch.load(model_path, map_location=torch.device('cpu'))
-    model.eval()  # Set the model to evaluation mode
-    return model
+def load_model():
+    return torch.load('checkpoint.pth', map_location=torch.device('cpu'))
 
 @st.cache_data
 def predict(img, model):
-    # Define the transformations
     transform = transforms.Compose([
         transforms.Resize(size=(512, 512)),
         transforms.ToTensor()
     ])
-    # Open the image and apply the transformations
     pil_image = Image.open(img).convert("RGB")
     img_tensor = transform(pil_image)
-    # Perform the prediction
-    with torch.no_grad():
-        output = model(img_tensor.unsqueeze(dim=0))
-    return output
+    return model(img_tensor.unsqueeze(dim=0))
 
-# Define the URL to the model file on GitHub
-model_url = 'https://github.com/Abhi-Shek17/Ai_image_classifier/tree/main/checkpoint.py'
-model_path = 'model.pth'
-
-# Download and load the model
-model = download_model(model_url, model_path)
-
-# File uploader to allow users to upload images
 img = st.file_uploader("Input your image", type=['jpg', 'png'])
 class_name = ['AI generated image', 'Real image']
 
 if img is not None:
-    # Make the prediction
-    idx = torch.argmax(predict(img, model)).item()
+    model = load_model()
+    idx = torch.argmax(predict(img, model))
     string = class_name[idx]
-    # Display the uploaded image
     st.image(img)
-    # Display the prediction result as a caption
+    # Display image and caption
     st.markdown(
         f"""
         <div class="image-container">
